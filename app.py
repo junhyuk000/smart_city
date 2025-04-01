06e86550b9f95b62be1812014a7f33d5f176bd4c
junhyuk000 <junhyuk000@gmail.com>
2025-04-01 11:22:16 +0900

    sos API URL 수정

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ received_data = {"message": "No data received"}
 
 # SOS 함수수
 def send_sos_alert_to_police(location, stream_url):
-    SOS_API_URL = "http://10.0.66.89:5002/sos_alert"
+    SOS_API_URL = "http://10.0.66.11:5002/sos_alert"
 
     data = {
         "type": "SOS",
